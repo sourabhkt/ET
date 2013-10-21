194bbd88ddcdb3476acfa79196b9416e243d5bc1
PagalGuy <sourabhkt@gmail.com>
2013-10-21 22:14:49 +0000

    Shri ganeshay namah :

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ET::Application.config.secret_key_base = '2cefccdff2a946ab9f282d344e52950a962ca6b02f05c37263d36f36c3ed8f625f4412dc8bfd989918c6b88fadabd5797fbf55979d9d9aa4da9d52aec54ce791'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ET::Application.config.session_store :cookie_store, key: '_ET_session'
